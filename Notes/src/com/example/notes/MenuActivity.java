ab8bbba170aa09d932799adbaf55081c123aca9a
Ozmagix <mystic_wild@hotmail.co.th>
2016-05-20 10:18:43 +0700

    Update MenuActivity.java

--- Notes/src/com/example/notes/MenuActivity.java
+++ Notes/src/com/example/notes/MenuActivity.java
@@ -1,9 +1,40 @@
+/*
+WOWW             WOW             WOWW             WOWWOWWOWWOWWOWWOWWOWWOW      WOWW             WOW             WOWW      !!!!!      !!!!!
+WOWW            WOWW            WOWW             WOWWOWWOWWOWWOWWOWWOWWOW       WOWW            WOWW            WOWW      !!!!!      !!!!!
+WOWW           WOWWO           WOWW             WOWWOW            WOWWOW        WOWW           WOWWO           WOWW      !!!!!      !!!!!
+WOWW          WOWWOW          WOWW             WOWWOW            WOWWOW         WOWW          WOWWOW          WOWW      !!!!!      !!!!!
+WOWW         WOWWWOW         WOWW             WOWWOW            WOWWOW          WOWW         WOWWWOW         WOWW      !!!!!      !!!!!
+WOWW        WOWWOWWO        WOWW             WOWWOW            WOWWOW           WOWW        WOWWOWWO        WOWW      !!!!!      !!!!!
+WOWW       WOWW WWOW       WOWW             WOWWOW            WOWWOW            WOWW       WOWW WWOW       WOWW      !!!!!      !!!!!
+WOWW      WOWW  WWOW      WOWW             WOWWOW            WOWWOW             WOWW      WOWW  WWOW      WOWW      !!!!!      !!!!!
+WOWW     WOWW   WWOW     WOWW             WOWWOW            WOWWOW              WOWW     WOWW   WWOW     WOWW      !!!!!      !!!!!
+WOWW    WOWW    WWOW    WOWW             WOWWOW            WOWWOW               WOWW    WOWW    WWOW    WOWW      !!!!!      !!!!!
+WOWW   WOWW     WWOW   WOWW             WOWWOW            WOWWOW                WOWW   WOWW     WWOW   WOWW      !!!!!      !!!!!
+WOWW  WOWW      WWOW  WOWW             WOWWOW            WOWWOW                 WOWW  WOWW      WWOW  WOWW      !!!!!      !!!!!
+WOWW WOWW       WWOW WOWW             WOWWOW            WOWWOW                  WOWW WOWW       WWOW WOWW      !!!!!      !!!!!
+WOWWOWWO        WWOWWOWW             WOWWOW            WOWWOW                   WOWWOWWO        WWOWWOWW      !!!!!      !!!!!
+WOWWOWW         WWOWWOW             WOWWOW            WOWWOW                    WOWWOWW         WWOWWOW
+WOWWOW          WWOWWO             WOWWOW            WOWWOW                     WOWWOW          WWOWWO
+WOWWO           OWWOW             WOWWOWWOWWOWWOWWOWWOWWOW                      WOWWO           OWWOW      !!!!!      !!!!!
+WOWW            OWWO             WOWWOWWOWWOWWOWWOWWOWWOW                       WOWW            OWWO      !!!!!      !!!!!
+*****************************************************
+* WOW WOW WOW group : NOTES & REMINDERS             *
+*                     member                        *
+* Miss  Nattida     Boonpae            56070501097  *
+* MR.   Thanawat    Wongpuak           56070501098  *
+* Miss  Panussaya   Sathitchaiwattana  56070501099  *
+* MR.   Pacharapon  Leewanitchakul     56070501100  *
+*****************************************************
+*/
 package com.example.notes;
-
+// ######  ###     ###  #####    ####   #####  ######
+//   ##    ####   ####  ##  ##  ##  ##  ##  ##   ##
+//   ##    ## ## ## ##  #####   ##  ##  #####    ##
+//   ##    ##  ###  ##  ##      ##  ##  ##  ##   ##
+// ######  ##   #   ##  ##       ####   ##   ##  ##
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -16,62 +47,70 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+//  ######  ##   ##  ###   ##   #####  ######  ######   ####   ###   ##
+//  ##      ##   ##  ####  ##  ##   ##   ##      ##    ##  ##  ####  ##
+//  ####    ##   ##  ## ## ##  ##        ##      ##    ##  ##  ## ## ##
+//  ##      ##   ##  ##  ####  ##   ##   ##      ##    ##  ##  ##  ####
+//  ##       #####   ##   ###   #####    ##      ##     ####   ##   ###
+
 public class MenuActivity extends Activity {
+	// this class is for Menu at top-right screen
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
 		
-		final ListView listview = (ListView) findViewById(R.id.listview);
+		final ListView listview = (ListView) findViewById(R.id.listview); // Setup id list view
 	    String[] values = new String[] { "Add Audit","Delete Audit","View Audit","Back"};
 
 	    final ArrayList<String> list = new ArrayList<String>();
 	    for (int i = 0; i < values.length; ++i) {
-	      list.add(values[i]);
+	      list.add(values[i]); // add 4 String in String[] to list
 	    }
 	    final StableArrayAdapter adapter = new StableArrayAdapter(this,
 	        android.R.layout.simple_list_item_1, list);
 	    listview.setAdapter(adapter);
 
 	    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	    	// can hear "CLICK!"
 
 	      @Override
 	      public void onItemClick(AdapterView<?> parent, final View view,
-	          int position, long id) {
-	    	  Bundle bundle = getIntent().getExtras();
-	    	  int year = bundle.getInt("Year");
+	          int position, long id) {                  // Keep Click position
+	    	  Bundle bundle = getIntent().getExtras();  // receive 3 values from pevious page
+	    	  int year = bundle.getInt("Year");         // "--------------------------------"
 	    	  int month = bundle.getInt("Month");
 	    	  int day = bundle.getInt("Day");
-	    	  final String item = (String) parent.getItemAtPosition(position);
-	    	  Toast.makeText(getApplicationContext(), item + " : " + day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+	    	  final String item = (String) parent.getItemAtPosition(position);   // Keep position
+	    	  Toast.makeText(getApplicationContext(), item + " : " + day + "/" + month + "/" + year, Toast.LENGTH_LONG).show(); //Black-box popup
 	    	  if(item.equalsIgnoreCase("Back")){
-	    		  finish();
+	    		  finish(); // CLICK Back button then go pevious page
 	    	  }else if(item.equalsIgnoreCase("Add Audit")){
-	    		  Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
-					Intent i = new Intent(getApplicationContext(), Add_audit.class);
-					i.putExtra("Year", year);
+	    		  Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show(); //Black-box popup
+					Intent i = new Intent(getApplicationContext(), Add_audit.class); // Prepare go to Add_audit class & page
+					i.putExtra("Year", year); // passing 3 values
 					i.putExtra("Month", month);
 					i.putExtra("Day", day);
-					startActivity(i);
+					startActivity(i); // GO!
 	    	  }else if(item.equalsIgnoreCase("Delete Audit")){
-	    		  	Intent i = new Intent(getApplicationContext(), Delete_audit.class);
-					i.putExtra("Year", year);
+	    		  	Intent i = new Intent(getApplicationContext(), Delete_audit.class); // Prepare go to Delete_audit class & page
+					i.putExtra("Year", year); // passing 3 values
 					i.putExtra("Month", month);
 					i.putExtra("Day", day);
-					startActivity(i);
+					startActivity(i); // GO!
 	    	  }else if(item.equalsIgnoreCase("View Audit")){
-	    		  Intent i = new Intent(getApplicationContext(), View_audit.class);
-					i.putExtra("Year", year);
+	    		  Intent i = new Intent(getApplicationContext(), View_audit.class); // Prepare go to View_audit class & page
+					i.putExtra("Year", year); // passing 3 values
 					i.putExtra("Month", month);
 					i.putExtra("Day", day);
-					startActivity(i);
+					startActivity(i); // GO!
 	    	  }
 	      }
 
 	    });
 	}
-
+	// explain in some pevious class
 	 private class StableArrayAdapter extends ArrayAdapter<String> {
 
 		    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
