c3d089b5bfc973999dda2e80eb8b5abb4a739510
Ozmagix <mystic_wild@hotmail.co.th>
2016-05-20 10:26:45 +0700

    Update View_audit.java

--- Notes/src/com/example/notes/View_audit.java
+++ Notes/src/com/example/notes/View_audit.java
@@ -1,10 +1,42 @@
+/*
+WOWW             WOW             WOWW             WOWWOWWOWWOWWOWWOWWOWWOW      WOWW             WOW             WOWW      !!!!!      !!!!!
+WOWW            WOWW            WOWW             WOWWOWWOWWOWWOWWOWWOWWOW       WOWW            WOWW            WOWW      !!!!!      !!!!!
+WOWW           WOWWO           WOWW             WOWWOW            WOWWOW        WOWW           WOWWO           WOWW      !!!!!      !!!!!
+WOWW          WOWWOW          WOWW             WOWWOW            WOWWOW         WOWW          WOWWOW          WOWW      !!!!!      !!!!!
+WOWW         WOWWWOW         WOWW             WOWWOW            WOWWOW          WOWW         WOWWWOW         WOWW      !!!!!      !!!!!
+WOWW        WOWWOWWO        WOWW             WOWWOW            WOWWOW           WOWW        WOWWOWWO        WOWW      !!!!!      !!!!!
+WOWW       WOWW WWOW       WOWW             WOWWOW            WOWWOW            WOWW       WOWW WWOW       WOWW      !!!!!      !!!!!
+WOWW      WOWW  WWOW      WOWW             WOWWOW            WOWWOW             WOWW      WOWW  WWOW      WOWW      !!!!!      !!!!!
+WOWW     WOWW   WWOW     WOWW             WOWWOW            WOWWOW              WOWW     WOWW   WWOW     WOWW      !!!!!      !!!!!
+WOWW    WOWW    WWOW    WOWW             WOWWOW            WOWWOW               WOWW    WOWW    WWOW    WOWW      !!!!!      !!!!!
+WOWW   WOWW     WWOW   WOWW             WOWWOW            WOWWOW                WOWW   WOWW     WWOW   WOWW      !!!!!      !!!!!
+WOWW  WOWW      WWOW  WOWW             WOWWOW            WOWWOW                 WOWW  WOWW      WWOW  WOWW      !!!!!      !!!!!
+WOWW WOWW       WWOW WOWW             WOWWOW            WOWWOW                  WOWW WOWW       WWOW WOWW      !!!!!      !!!!!
+WOWWOWWO        WWOWWOWW             WOWWOW            WOWWOW                   WOWWOWWO        WWOWWOWW      !!!!!      !!!!!
+WOWWOWW         WWOWWOW             WOWWOW            WOWWOW                    WOWWOWW         WWOWWOW
+WOWWOW          WWOWWO             WOWWOW            WOWWOW                     WOWWOW          WWOWWO
+WOWWO           OWWOW             WOWWOWWOWWOWWOWWOWWOWWOW                      WOWWO           OWWOW      !!!!!      !!!!!
+WOWW            OWWO             WOWWOWWOWWOWWOWWOWWOWWOW                       WOWW            OWWO      !!!!!      !!!!!
+*****************************************************
+* WOW WOW WOW group : NOTES & REMINDERS             *
+*                     member                        *
+* Miss  Nattida     Boonpae            56070501097  *
+* MR.   Thanawat    Wongpuak           56070501098  *
+* Miss  Panussaya   Sathitchaiwattana  56070501099  *
+* MR.   Pacharapon  Leewanitchakul     56070501100  *
+*****************************************************
+*/
 package com.example.notes;
+
+// ######  ###     ###  #####    ####   #####  ######
+//   ##    ####   ####  ##  ##  ##  ##  ##  ##   ##
+//   ##    ## ## ## ##  #####   ##  ##  #####    ##
+//   ##    ##  ###  ##  ##      ##  ##  ##  ##   ##
+// ######  ##   #   ##  ##       ####   ##   ##  ##
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -18,7 +50,14 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+//  ######  ##   ##  ###   ##   #####  ######  ######   ####   ###   ##
+//  ##      ##   ##  ####  ##  ##   ##   ##      ##    ##  ##  ####  ##
+//  ####    ##   ##  ## ## ##  ##        ##      ##    ##  ##  ## ## ##
+//  ##      ##   ##  ##  ####  ##   ##   ##      ##    ##  ##  ##  ####
+//  ##       #####   ##   ###   #####    ##      ##     ####   ##   ###
+
 public class View_audit extends Activity {
+	// This class is use for show ALL audit in one day and Calculate Total Audit's price
 	private ArrayList<String> DayAudit;
 	private DBHelper mydb;
 	private int year;
@@ -28,44 +67,44 @@ public class View_audit extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_view_audit);
-		Bundle bundle = getIntent().getExtras();
+		setContentView(R.layout.activity_view_audit); // Setup page
+		Bundle bundle = getIntent().getExtras();      // receive 3 values
    	  	year = bundle.getInt("Year");
    	  	month = bundle.getInt("Month");
    	  	day = bundle.getInt("Day");
-		mydb = new DBHelper(this);
-		DayAudit = mydb.getDayAudit(year,month,day);
-		DayAudit.add("Total");
-		DayAudit.add("Back");
+		mydb = new DBHelper(this);                    // Set Database
+		DayAudit = mydb.getDayAudit(year,month,day);  // Get All Audit
+		DayAudit.add("Total");                        // add Total Button
+		DayAudit.add("Back");                         // add Back button
 		
 		
 		final ListView listview = (ListView) findViewById(R.id.listview);
-		
+		// use list Day Audit to Create List of LAYOUT
 	    final StableArrayAdapter adapter = new StableArrayAdapter(this,
 	        android.R.layout.simple_list_item_1, DayAudit);
 	    listview.setAdapter(adapter);
 
-	    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() { // Can hear "CLICK!"
 
 	      @Override
 	      public void onItemClick(AdapterView<?> parent, final View view,
-	          int position, long id) {
-	  
+	          int position, long id) { 
+	          // CLICK keep position
 	    	  final String item = (String) parent.getItemAtPosition(position);
 	    	  if(item.equalsIgnoreCase("Back")){
-	    		  finish();
+	    		  finish(); // CLICK Back then go back
 	    	  }else if(item.equalsIgnoreCase("Total")){
-	    		  showCheckDialog();  
-	    	  }else{
+	    		  showCheckDialog();  // CLICK Total to calculate total price of the day
+	    	  }else{ // CLICK other look position
 	    	  view.animate().setDuration(2000).alpha(0)
 	            .withEndAction(new Runnable() {
 	              @Override
 	              public void run() {
-	                DayAudit.remove(item);
+	                DayAudit.remove(item); // remove from screen
 	                String [] str = item.split("\t");
-	                mydb.deleteAudit(str[0], str[1], str[2], year, month, day);
+	                mydb.deleteAudit(str[0], str[1], str[2], year, month, day); // DELETE from database
 	                
-	                adapter.notifyDataSetChanged();
+	                adapter.notifyDataSetChanged(); // Notify
 	                view.setAlpha(1);
 	              }
 	            });
@@ -75,24 +114,24 @@ public class View_audit extends Activity {
 	    });
 	}
 
-	private void showCheckDialog() {
-        AlertDialog.Builder showAuditAlert = new AlertDialog.Builder(this);
-        showAuditAlert.setTitle("Total prize");
-        showAuditAlert.setMessage("= "+mydb.getResultDayAudit(year,month,day).intValue()+"");
+	private void showCheckDialog() { // Show total price CLASS
+        AlertDialog.Builder showAuditAlert = new AlertDialog.Builder(this); // Create Alert dialog
+        showAuditAlert.setTitle("Total prize"); // Set title
+        showAuditAlert.setMessage("= "+mydb.getResultDayAudit(year,month,day).intValue()+""); // Set message
 
         showAuditAlert.setPositiveButton("Done",
                 new DialogInterface.OnClickListener()
-                {
+                { // Can hear "CLICK"
                     @Override
                     public void onClick(DialogInterface dialog,int which)
                     {
-                    	dialog.dismiss();
+                    	dialog.dismiss(); // CLICK then disappear
                     }
                 }
         );
-        showAuditAlert.show();
+        showAuditAlert.show(); // Show dialog that created upper this line
     }
-	
+	// Explained in some pevious class
 	 private class StableArrayAdapter extends ArrayAdapter<String> {
 
 		    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
